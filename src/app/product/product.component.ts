9a6b95f9caf3d2e58788b8a81bbd8d2d1618717f
rohitghosh86 <rohit.the.coder@gmail.com>
2018-06-28 15:54:05 +0530

    Add files via upload

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers} from '@angular/http'
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor(private http: Http) { }
+  
+  confirmationString:string = "New Product has been added";
+  isAdded:boolean = false;
+  
+  productObj:object = {};
+
+  addNewProduct = function(product) {
+    this.productObj = {
+      "name": product.name,
+      "color": product.color
+    }
+    this.http.post("http://localhost:5555/products/", this.productObj)
+    .subscribe((res:Response) => {
+      this.isAdded=true;
+      console.log(res);
+    }) 
+  }
+
+  ngOnInit() {
+  }
+
+}
